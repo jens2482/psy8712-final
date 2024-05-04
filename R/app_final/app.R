3da65039c9b1d25f33c92110878926bc585237fb
Amanda Jensen <jens2482@umn.edu>
2024-05-04 13:37:36 -0500

    App done! Woof.

--- R/app_final/app.R
+++ R/app_final/app.R
@@ -9,138 +9,149 @@ library(ggplot2) #need for visualizations in app
 clean_tas_data <- read_csv (file = '../../out/clean_tas_data.csv') #import skinny file that was created in original R script
 
 # Define UI 
-ui <- fluidPage(
-
-    # app title
-    titlePanel("Student Think-Aloud Analysis"),
-
-    # sidebar with radio buttons for all options to filter
-    sidebarLayout(
-        sidebarPanel(
-          
-          #create set of radio buttons for selecting condition
-          radioButtons("conditionselect",
-                       label="Which condition would you like to see?",
-                       choices=c("Consecutive","Iterative","All"),
-                       selected="All"),
-          
-          #create set of radio buttons for selecting gender
-          radioButtons("genderselect",
-                       label="Which gender would you like to see?",
-                       choices=c("Male","Female","All"),
-                       selected="All"),
-          
-          #create set of radio buttons for selecting grade
-          radioButtons("gradeselect",
-                       label="Which grade would you like to see?",
-                       choices=c("4","5","All"),
-                       selected="All"),
-          
-          #create set of radio buttons for selecting ML status
-          radioButtons("mlselect",
-                       label="Would you like to filter results for multilingual learners (MLs)?",
-                       choices=c("Only show MLs","Show all students except MLs","Show all students"),
-                       selected="Show all students"),
-        ),
-        
-        # main panel for displaying analyses - different tabs based on the different analyses
-        mainPanel(   #display analyses in main panel
-          column(12, #make it the full width of the main panel (not sure if this is necessary)
-                 tabsetPanel(   #set up the main panel so that users can click between different panels
-                   
-                   #create panel for descriptives
-                   tabPanel("Descriptives", 
-                            h3("Descriptive Statistics"), 
-                            tableOutput("descTable")),
-                   
-                   #create panel for frequencies
-                   tabPanel("Frequencies", 
-                            h3("Frequency Analysis"), 
-                            plotOutput("freqPlot")),
-                   
-                   #create panel for sentiments
-                   tabPanel("Sentiment", 
-                            h3("Sentiment Analysis"), 
-                            plotOutput("sentPlot")),
-                   
-                   #create panel for correlations
-                   tabPanel("Correlations", 
-                            h3("Correlation Analysis"), 
-                            plotOutput("corrPlot"))
-                 )
-          )
-        )
+ui <- fluidPage( #I believe this makes it so that it adjusts based on the size of the screen viewing your app
+  
+  # app title
+  titlePanel("Student Think-Aloud Analysis"), 
+  
+  # sidebar with radio buttons for all options to filter
+  sidebarLayout(
+    sidebarPanel(  #wanted to the buttons to be in a sidebar so they're not the focus but are still obvious
+      
+      #create set of radio buttons for selecting condition - individual line comments are the same for each set of radio buttons
+      radioButtons("conditionselect", #reference to use when filtering for this set of buttons
+                   label="Which condition would you like to see?", #displayed question for user
+                   choices=c("Consecutive","Iterative","All"), #options to choose between
+                   selected="All"), #default option
+      
+      #create set of radio buttons for selecting gender
+      radioButtons("genderselect",
+                   label="Which gender would you like to see?",
+                   choices=c("Male","Female","All"),
+                   selected="All"),
+      
+      #create set of radio buttons for selecting grade
+      radioButtons("gradeselect",
+                   label="Which grade would you like to see?",
+                   choices=c("4","5","All"),
+                   selected="All"),
+      
+      #create set of radio buttons for selecting ML status
+      radioButtons("mlselect",
+                   label="Would you like to filter results for multilingual learners (MLs)?",
+                   choices=c("Only show MLs","Show all students except MLs","Show all students"),
+                   selected="Show all students"),
+    ),
+    
+    # main panel for displaying analyses - different tabs based on the different analyses
+    mainPanel(   #display analyses in main panel
+      column(12, #make it the full width of the main panel (not sure if this is necessary)
+             tabsetPanel(   #set up the main panel so that users can click between different panels
+               
+               #create panel for descriptives - individual line comments are the same for all panels
+               tabPanel("Descriptives",    #name of panel
+                        h3("Descriptive Statistics"), #heading that will display
+                        tableOutput("descTable")), #output that will be displayed - is defined later in the server logic area
+               
+               #create panel for frequencies
+               tabPanel("Frequencies", 
+                        h3("Frequency Analysis"), 
+                        plotOutput("freqPlot")),
+               
+               #create panel for sentiments
+               tabPanel("Sentiment", 
+                        h3("Sentiment Analysis"), 
+                        plotOutput("sentPlot"))
+             )
+      )
     )
+  )
 )
 
 
 # Define server logic
 server <- function(input, output) {
- 
+  
   # define data based on filters for later use
   filtered_data <- reactive({ #create a reactive function called filtered data where it depends on what radio buttons were selected
-    data <- clean_tas_data
+    data <- clean_tas_data #use clean data file but just refer to it as data in later functions for simplicity
     
     #filter for condition
-    if (input$conditionselect != "All") {
+    if (input$conditionselect != "All") {  #if anything is selected other than "All"...
       data <- data %>% 
-        filter(Condition == input$conditionselect)
+        filter(Condition == input$conditionselect)  #filter the data based on the values in the "condition" column
     }
     
     #filter for gender
-    if (input$genderselect != "All") {
-      gender_map <- c("Male" = "M", "Female" = "F")
+    if (input$genderselect != "All") { #if anything is selected other than "All"...
+      gender_map <- c("Male" = "M", "Female" = "F")  #values in column didn't match values users are choosing between so needed to connect "Male" to "M" in the dataset and same for female
       data <- data %>% 
-        filter(Gender %in% gender_map[input$genderselect])
+        filter(Gender %in% gender_map[input$genderselect]) #filter the data based on the values in the "gender" column
     }
     
     #filter for grade
-    if (input$gradeselect != "All") {
+    if (input$gradeselect != "All") { #if anything is selected other than "All"...
       data <- data %>% 
-        filter(Grade == input$gradeselect)
+        filter(Grade == input$gradeselect) #filter the data based on the values in the "grade" column
     }
     
     #filter for multilingual learner status
-    if (input$mlselect == "Only show MLs") {
+    if (input$mlselect == "Only show MLs") { #if "Only show MLs" is selected...
       data <- data %>% 
-        filter(ML == "Y")
-    } else if (input$mlselect == "Show all students except MLs") {
+        filter(ML == "Y") #filter so only those with Y tag for MLs are shown
+    } else if (input$mlselect == "Show all students except MLs") { #if "Show all students except MLs" is selected...
       data <- data %>% 
-        filter(ML == "N")
+        filter(ML == "N") #filter so only those with N tag for MLs are shown
     }
     
     data #output the filtered results
   })
   
+  #create output for the "Descriptives" panel
+  output$descTable <- renderTable({ #defines descTable output that is placed in the "Frequencies" panel in UI
+
+    # Compute the descriptive statistics 
+    descriptives <- filtered_data() %>%  #create name for new dataset that will be used in table output
+      summarize(
+        "Average Words Per Sentence" = mean(words_per_sentence), # average number of words per sentence
+        "SD Words Per Sentence" = sd(words_per_sentence),# SD of words per sentence
+        "Average Length of Word" = mean(num_ltrs), #average length of words
+        "SD Length of Word" = sd(num_ltrs) #sd length of words
+      )
+    
+    # Return the table with descriptives
+    descriptives #returns descriptives table as an output - not the prettiest but gets the point across
+    
+  })
+  
   #create output for the "Frequencies" panel
-  output$freqPlot <- renderPlot({
+  output$freqPlot <- renderPlot({   #defines freqPlot output that is placed in the "Frequencies" panel in UI
     # Use the filtered data to count word occurrences
     word_data <- filtered_data() %>%
       filter(stop == "N") %>%  # only include non-stop words
-      count(word = word, sort = TRUE) %>%
+      count(word = word, sort = TRUE) %>% #sort by highest frequency
       top_n(20, n) # select top 20 most frequent words
-
-    # create bar plot
+    
+    #create bar plot
     ggplot(word_data, aes(x = reorder(word, n), y = n)) + #display in descending order of words
-      geom_bar(stat = "identity", fill = "blue") + #display bars in blue
-      labs(x = "Word", y = "Frequency", title = "Most Used Words") +
-      theme_minimal() + #gets rid of grayish backgroun on plot that I don't love
+      geom_bar(stat = "identity", fill = "red") + #display bars in red
+      labs(x = "Word", y = "Frequency", title = "Most Used Words (Not Including Stop Words)") + #add titles to axes and overall
+      theme_minimal() + #gets rid of grayish background on plot that I don't love
       coord_flip() # flip axes to read words better
   })
   
   #create output for the "Sentiment" panel
-  output$sentPlot <- renderPlot({
+  output$sentPlot <- renderPlot({ #defines sentPlot output that is placed in the "Sentiment" panel in UI
     # Use the filtered data to count sentiment occurrences
     sentiment_data <- filtered_data() %>%
       filter(sentiment != "N/A") %>%  # get rid of N/As
-      count(sentiment = sentiment, sort = TRUE) %>% 
-      top_n(20, n) # Select top 20 sentiments based on frequency
+      count(sentiment = sentiment, sort = TRUE) #sort by highest frequency
     
-    # create bar plot
+    #create bar plot
     ggplot(sentiment_data, aes(x = reorder(sentiment, n), y = n)) + #display in descending order of sentiments
       geom_bar(stat = "identity", fill = "blue") + #display bars in blue
-      labs(x = "Sentiment", y = "Frequency", title = "Most Used Words") +
-      theme_minimal() + #gets rid of grayish backgroun on plot that I don't love
+      labs(x = "Sentiment", y = "Frequency", title = "Most Used Sentiments from NRC Dictionary") + #add titles to axes and overall
+      theme_minimal() + #gets rid of grayish background on plot that I don't love
       coord_flip() # flip axes to read words better
   })
 }
--- R/final.R
+++ R/final.R
@@ -2,7 +2,6 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #set working directory
 library(tidyverse) #use tidyverse for readr, dplyr
 library(tidytext) #needed for NLP/text mining functions
-library(textdata) #needed for loughran sentiment dictionary
 
 # Data Import and Cleaning
 tas_data <- read_csv (file = '../data/tas_data.csv') #use tidyverse to get tbl to work better with dplyr later
@@ -13,13 +12,27 @@ sentiment <- get_sentiments("nrc") %>% #get list of nrc sentiments
   ungroup() #get rid of grouping
   
 #create data version tokenzied by word
-clean_tas_data <- tas_data %>%
+clean_tas_data <- tas_data %>%  #rename after pre-processing/cleaning
   unnest_tokens(word, Response) %>% #tokenize the items from the Response column and call that column word; removes punctuation and makes lowercase too
   mutate(num_ltrs = nchar(word)) %>% #add a column that shows the total number of characters in each word
   left_join(stop_words, by = "word") %>% #keep all columns from both but just values from the original dataset - want to be able to indicate which words are stop words so I can filter for them later rather than creating a separate tbl with stop words removed
-  mutate(stop = if_else(is.na(lexicon), "N", if_else(lexicon == "snowball", "Y", "Other"))) %>% #add a column called stop that tracks whether the word in the word column is a stop word or not - if the column "lexicon" had a value "snowball"  (meaning it came from the stop word list) add a "Y" to this column so we can filter for it later, if not it prints "N"
+  mutate(stop = if_else(is.na(lexicon), "N", if_else(lexicon == "snowball", "Y", "Other"))) %>% #add a column called stop that tracks whether the word in the word column is a stop word or not - if the column "lexicon" had a value "snowball"  (meaning it came from the stop word list) add a "Y" to this column so we can filter for it later, if not it prints "N". There are some things in my app that I want to look at the raw words used so I don't always want to get rid of stop words, just want to set it up for an option to filter it.
   left_join(sentiment, by = "word") %>%  #join with sentiment dictionary to assign sentiments to words
-  select(Username, Condition, Grade, Gender, Age, ML, WMCwp, GMRTcp, Genre, Sentence, word, num_ltrs, stop, sentiment) #only select desired columns so that it takes shiny less time to run
-  
+  select(Username, Condition, Grade, Gender, ML, Sentence, word, num_ltrs, stop, sentiment) #only select desired columns so that it takes shiny less time to run
+
+#create a summary of total words per sentence to add to dataset
+sentence_level <- clean_tas_data %>% #create intermediate new dataset
+  select(Username,Sentence,word) %>% #only want these three columns for when I join it later
+  group_by(Username, Sentence) %>% #only calculate within these groupings
+  summarize(
+    words_per_sentence = n() # average number of words per sentence by counting rows (words) within the groupings of Username and Sentence
+  )
+
+#add words per sentence onto data
+clean_tas_data <- clean_tas_data %>%  #replace original dataset with this new version
+  left_join(sentence_level, by = c("Username", "Sentence")) #adds wps amount to current dataset - I need this for calculating descriptives in my app
+
+ 
 # Data Export
 write_csv(clean_tas_data, "../out/clean_tas_data.csv") #save into out folder since it's a cleaned file
+
