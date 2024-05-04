a67aacda3bcc03a86bcc284e67ce85e672fe14d1
Amanda Jensen <jens2482@umn.edu>
2024-05-04 10:50:00 -0500

    lots accomplished! created app filters and filter outputs as well as word frequency table

--- R/app_final/app.R
+++ R/app_final/app.R
@@ -1,48 +1,131 @@
 # Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #set working directory
 library(shiny) #need to run this app
+library(dplyr) #need for running some analyses in app
+library(ggplot2) #need for visualizations in app
 
-#Data Import and Cleaning
-#import data
-tas_data <- read_csv (file = '../../data/tas_data.csv') #import skinny file that was created in original R script
-library(ggplot) #need for visualizations
+# Data Import and Cleaning
+# import data
+clean_tas_data <- read_csv (file = '../../out/clean_tas_data.csv') #import skinny file that was created in original R script
 
-#Visualization
 # Define UI 
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Think-Aloud Utterance Analysis"),
+    # app title
+    titlePanel("Student Think-Aloud Analysis"),
 
-    # Sidebar with a slider input for number of bins 
+    # sidebar with radio buttons for all options to filter
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          
+          #create set of radio buttons for selecting condition
+          radioButtons("conditionselect",
+                       label="Which condition would you like to see?",
+                       choices=c("Consecutive","Iterative","All"),
+                       selected="All"),
+          
+          #create set of radio buttons for selecting gender
+          radioButtons("genderselect",
+                       label="Which gender would you like to see?",
+                       choices=c("Male","Female","All"),
+                       selected="All"),
+          
+          #create set of radio buttons for selecting grade
+          radioButtons("gradeselect",
+                       label="Which grade would you like to see?",
+                       choices=c("4","5","All"),
+                       selected="All"),
+          
+          #create set of radio buttons for selecting ML status
+          radioButtons("mlselect",
+                       label="Would you like to filter results for multilingual learners (MLs)?",
+                       choices=c("Only show MLs","Show all students except MLs","Show all students"),
+                       selected="Show all students"),
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+        
+        # main panel for displaying analyses - different tabs based on the different analyses
+        mainPanel(   #display analyses in main panel
+          column(12, #make it the full width of the main panel (not sure if this is necessary)
+                 tabsetPanel(   #set up the main panel so that users can click between different panels
+                   
+                   #create panel for descriptives
+                   tabPanel("Descriptives", 
+                            h3("Descriptive Statistics"), 
+                            tableOutput("descTable")),
+                   
+                   #create panel for frequencies
+                   tabPanel("Frequencies", 
+                            h3("Frequency Analysis"), 
+                            plotOutput("freqPlot")),
+                   
+                   #create panel for sentiments
+                   tabPanel("Sentiment", 
+                            h3("Sentiment Analysis"), 
+                            plotOutput("sentPlot")),
+                   
+                   #create panel for correlations
+                   tabPanel("Correlations", 
+                            h3("Correlation Analysis"), 
+                            plotOutput("corrPlot"))
+                 )
+          )
         )
     )
 )
 
-# Define server logic required to draw a histogram
+
+# Define server logic
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+ 
+  # define data based on filters for later use
+  filtered_data <- reactive({ #create a reactive function called filtered data where it depends on what radio buttons were selected
+    data <- clean_tas_data
+    
+    #filter for condition
+    if (input$conditionselect != "All") {
+      data <- data %>% 
+        filter(Condition == input$conditionselect)
+    }
+    
+    #filter for gender
+    if (input$genderselect != "All") {
+      gender_map <- c("Male" = "M", "Female" = "F")
+      data <- data %>% 
+        filter(Gender %in% gender_map[input$genderselect])
+    }
+    
+    #filter for grade
+    if (input$gradeselect != "All") {
+      data <- data %>% 
+        filter(Grade == input$gradeselect)
+    }
+    
+    #filter for multilingual learner status
+    if (input$mlselect == "Only show MLs") {
+      data <- data %>% 
+        filter(ML == "Y")
+    } else if (input$mlselect == "Show all students except MLs") {
+      data <- data %>% 
+        filter(ML == "N")
+    }
+    
+    data #output the filtered results
+  })
+  
+  #create output for the "Frequencies" panel (Qualitative)
+  output$freqPlot <- renderPlot({
+    # Use the filtered data to count word occurrences
+    word_data <- filtered_data() %>%
+      count(word = word, sort = TRUE) %>% # replace 'word' with actual column name if different
+      top_n(20, n) # Select top 20 words based on frequency
+    
+    # create bar plot
+    ggplot(word_data, aes(x = reorder(word, n), y = n)) +
+      geom_bar(stat = "identity", fill = "blue") +
+      labs(x = "Word", y = "Frequency", title = "Most Used Words") +
+      theme_minimal() +
+      coord_flip() # Flip the axes for better readability of words
+  })
 }
 
 # Run the application 
--- R/final.R
+++ R/final.R
@@ -3,8 +3,8 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #set working directo
 library(tidyverse) #use tidyverse for readr, dplyr
 library(tidytext) #needed for NLP/text mining functions
 
-#Data Import and Cleaning
-tas_data <- read_csv (file = '../../data/tas_data.csv') #use tidyverse to get tbl to work better with dplyr later
+# Data Import and Cleaning
+tas_data <- read_csv (file = '../data/tas_data.csv') #use tidyverse to get tbl to work better with dplyr later
 stop_words <- get_stopwords() #get list of tidytext stop_words
 
 #create data version tokenzied by word
@@ -15,5 +15,5 @@ clean_tas_data <- tas_data %>%
   mutate(stop = if_else(lexicon == "snowball", "Y", lexicon)) %>% #add a column called stop that tracks whether the word in the word column is a stop word or not - if the column "lexicon" had a value "snowball"  (meaning it came from the stop word list) add a "Y" to this column so we can filter for it later
   select(Username, Condition, Grade, Gender, Age, ML, WMCwp, GMRTcp, Genre, Sentence, word, num_ltrs, stop) #only select desired columns so that it takes shiny less time to run
   
-#Data Export
-write_csv(clean_tas_data, "../../out/clean_tas_data.csv") #save into out folder since it's a cleaned file
+# Data Export
+write_csv(clean_tas_data, "../out/clean_tas_data.csv") #save into out folder since it's a cleaned file
